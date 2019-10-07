be9c1533d6eff5c8e5d19383ae275339a92539d1
earminjon <earminjon@vente-privee.com>
2019-10-07 09:41:58 +0800

    first

---
+++ app/src/main/java/com/example/enguerrand_robin_benjamin/EnguerrandRobinBenjamin.java
@@ -0,0 +1,13 @@
+package com.example.enguerrand_robin_benjamin;
+
+import android.app.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnguerrandRobinBenjamin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
